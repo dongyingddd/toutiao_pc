1cae8e5b9ce9155feb8a345092da724233057dbe
dongyingddd <2273010094@qq.comgit config --global user.email 2273010094@qq.com>
2020-03-02 22:08:09 +0800

    主页模块-页面布局完成

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,22 @@
 <template>
   <div>
-      <span>我是主页</span>
+    <!-- 外层大容器 -->
+      <el-container>
+        <!-- 左边容器 -->
+        <el-aside width="200px">
+          侧边栏导航
+        </el-aside>
+        <!-- 右侧大容器 -->
+        <el-container>
+          <!-- 顶部 -->
+          <el-header>顶栏容器</el-header>
+          <!-- 中部区域 -->
+          <el-main>
+            <!-- 二级路由容器 -->
+            <router-view></router-view>
+          </el-main>
+        </el-container>
+      </el-container>
   </div>
 </template>
 
