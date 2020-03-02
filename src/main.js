0a5f8dfc730ed064839df3d1197f0b98320c30ec
dongyingddd <2273010094@qq.comgit config --global user.email 2273010094@qq.com>
2020-03-02 21:42:27 +0800

    安装axios-配置全局使用

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import router from './router'
 import ElementUI from 'element-ui' // 引入element-ui
 import 'element-ui/lib/theme-chalk/index.css' // 引入样式
 import '@/styles/index.less' // 引入样式
+import axios from 'axios' // 引入axios
+axios.defaults.baseURL = 'http://ttapi.research.itcast.cn/mp/v1_0' // 配置公共的请求地址
+Vue.prototype.$axios = axios // 将axios设置给Vue对象的原型属性
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 new Vue({
