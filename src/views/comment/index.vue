6e312451d0dc04b124a9170e1bf8383bd85473b6
dongyingddd <2273010094@qq.comgit config --global user.email 2273010094@qq.com>
2020-03-05 21:02:16 +0800

    评论数据加载到页面以及评论状态的数据过滤

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -7,11 +7,14 @@
 
         <!-- 放置一个表格组件 -->
         <el-table :data="list" >
-            <el-table-column prop="" label="标题" width="500" ></el-table-column>
-            <el-table-column prop="" label="评论状态"></el-table-column>
-            <el-table-column prop="" label="总评论数"></el-table-column>
-            <el-table-column prop="" label="粉丝评论数"></el-table-column>
-            <el-table-column label="操作"></el-table-column>
+            <el-table-column prop="title" label="标题" width="500" ></el-table-column>
+            <el-table-column prop="comment_status" label="评论状态" :formatter="formatterTool"></el-table-column>
+            <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+            <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+            <el-table-column label="操作">
+                <el-button size="small" type="text">修改</el-button>
+                <el-button size="small" type="text">关闭评论</el-button>
+            </el-table-column>
         </el-table>
     </el-card>
 
@@ -23,6 +26,32 @@ export default {
     return {
       list: []
     }
+  },
+  methods: {
+    getList () {
+      this.$axios({
+        url: '/articles',
+        params: {
+          response_type: 'comment'
+        },
+        method: 'GET'
+      }).then((result) => {
+        this.list = result.data.results
+        console.log(result.data.results)
+      })
+    },
+    formatterTool (row, column, cellValue, index) {
+      // row : 代表当前一行数据
+      // column: 代表当前列信息
+      // cellValue: 代表当前单元格的值
+      // index: 代表当前索引
+      // 该函数需要一个返回值用来显示
+      // debugger
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    this.getList()
   }
 
 }
