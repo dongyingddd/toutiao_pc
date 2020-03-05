8b983c844c9a1960f512607c1bb2fe97ee0c7f53
dongyingddd <2273010094@qq.comgit config --global user.email 2273010094@qq.com>
2020-03-05 22:36:36 +0800

    评论列表-打开和关闭评论

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,7 +1,7 @@
 <template>
     <el-card>
         <!-- 放置一个面包屑组件 -->
-        <bread-crumb>
+        <bread-crumb slot="header">
             <span slot="title">评论管理</span>
         </bread-crumb>
 
@@ -12,8 +12,13 @@
             <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
             <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
             <el-table-column label="操作">
-                <el-button size="small" type="text">修改</el-button>
-                <el-button size="small" type="text">关闭评论</el-button>
+                <!-- 自定义列的作用域插槽 -->
+                <!-- el-table-column组件 在插槽中传出了 row $index store column  -->
+                <!-- 作用域插槽->子组件中的数据通过插槽传出 slot-scope接收 row(行数据) $index(索引)-->
+               <template slot-scope="obj">
+                    <el-button size="small" type="text">修改</el-button>
+                    <el-button size="small" type="text" @click='openOrClose(obj.row)'>{{obj.row.comment_status? '关闭':'打开'}}评论</el-button>
+               </template>
             </el-table-column>
         </el-table>
     </el-card>
@@ -28,6 +33,7 @@ export default {
     }
   },
   methods: {
+    // 加载评论数据
     getList () {
       this.$axios({
         url: '/articles',
@@ -37,9 +43,9 @@ export default {
         method: 'GET'
       }).then((result) => {
         this.list = result.data.results
-        console.log(result.data.results)
       })
     },
+    // 过滤评论状态
     formatterTool (row, column, cellValue, index) {
       // row : 代表当前一行数据
       // column: 代表当前列信息
@@ -48,6 +54,33 @@ export default {
       // 该函数需要一个返回值用来显示
       // debugger
       return cellValue ? '正常' : '关闭'
+    },
+    // 打开或关闭评论
+    openOrClose (row) {
+      const mess = row.comment_status ? '关闭' : '打开'
+      this.$confirm(`您确定${mess}评论吗?`, '提示')
+        .then(() => {
+          this.$axios({
+            url: '/comments/status',
+            // query参数
+            params: {
+              article_id: row.id // 要求参数的文章id
+            },
+            // body参数
+            data: {
+              allow_comment: !row.comment_status // 是打开还是关闭,此状态和文章评论状态相反
+            },
+            method: 'put'
+          }).then(() => {
+            // 成功了,提示个消息
+            this.$message.success(`${mess}评论成功!`)
+            // 然后重新拉取数据
+            this.getList()
+          }).catch(() => {
+            // 失败了会进入到catch
+            this.$message.error(`${mess}评论失败!`)
+          })
+        })
     }
   },
   created () {
