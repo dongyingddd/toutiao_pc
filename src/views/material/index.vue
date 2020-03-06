cdbe5f0fc01861b831cd2adafe27a9ba8e0085c8
dongyingddd <2273010094@qq.comgit config --global user.email 2273010094@qq.com>
2020-03-06 21:46:35 +0800

    素材列表-页面结构-tal

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -4,12 +4,26 @@
         <bread-crumb slot="header">
            <template slot="title">图片管理</template>
         </bread-crumb>
+        <!-- 放置标签页 -->
+        <!-- v-model绑定的属性就是tal默认激活的页签 -->
+        <el-tabs v-model="activeName">
+            <el-tab-pane label="全部" name="all">
+                全部素材
+            </el-tab-pane>
+            <el-tab-pane label="收藏" name="collect">
+                收藏素材
+            </el-tab-pane>
+        </el-tabs>
     </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      activeName: 'all'
+    }
+  }
 }
 </script>
 
